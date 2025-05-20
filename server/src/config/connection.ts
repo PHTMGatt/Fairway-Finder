97cff52675bad5e355a01256a4333bc5f92d0ece
Goose_tG <jarredhampton@hotmail.com>
2025-05-20 19:58:11 -0400

    update: github actions to include all branches

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -7,6 +7,7 @@ const MONGODB_URI = process.env.MONGODB_URI || '';
 
 const db = async (): Promise<typeof mongoose.connection> => {
   try {
+    console.log('Connecting to MongoDB with URI:', MONGODB_URI);
     await mongoose.connect(MONGODB_URI);
     console.log('Database connected.');
     return mongoose.connection;
